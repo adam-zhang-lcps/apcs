95eba75ef48fc557535829c2e0f094d39bfc1d66
Ad <me@adamorsomething.xyz>
2022-11-30 11:53:30 -0500

    OddDigits

---
+++ quarter-2/OddDigits.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Main {
+  public static void oddDigits(String cur, int len, ArrayList<String> arr) {
+    if (cur.length() == len) {
+      arr.add(cur);
+    } else {
+      for (int i = 1; i <= 9; i += 2) {
+        oddDigits(cur + i, len, arr);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    ArrayList<String> combo = new ArrayList<String>();
+    oddDigits("", 3, combo);
+    for (String temp : combo) {
+      System.out.println(temp);
+    }
+  }
+}
