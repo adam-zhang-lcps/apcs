a6a10b7fbfba034d3c9dd5bdb743358724d77f45
Ad <me@adamorsomething.xyz>
2022-11-30 11:49:59 -0500

    LeftRight

---
+++ quarter-2/LeftRight.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class Main {
+  public static void lr(String cur, int len, ArrayList<String> arr) {
+    if (cur.length() == len) {
+      arr.add(cur);
+    } else {
+      lr(cur + "L", len, arr);
+      lr(cur + "R", len, arr);
+    }
+  }
+
+  public static void main(String[] args) {
+    ArrayList<String> combo = new ArrayList<String>();
+    lr("", 3, combo);
+    for (String temp : combo) {
+      System.out.println(temp);
+    }
+  }
+}
